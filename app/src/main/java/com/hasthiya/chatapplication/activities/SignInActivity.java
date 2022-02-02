c02aacc35cb78acdab0c35511cc3080fd91c5311
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-02-02 15:50:45 +0530

    Done to Basic SignIN & SignUp Layout Design

--- app/src/main/java/com/hasthiya/chatapplication/activities/SignInActivity.java
+++ app/src/main/java/com/hasthiya/chatapplication/activities/SignInActivity.java
@@ -2,15 +2,27 @@ package com.hasthiya.chatapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.hasthiya.chatapplication.R;
+import com.hasthiya.chatapplication.databinding.ActivitySigninBinding;
 
 public class SignInActivity extends AppCompatActivity {
+    
+    private ActivitySigninBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+
+        binding = ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
