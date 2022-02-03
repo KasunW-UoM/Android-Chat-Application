c50027539735f4e40a4d24aa4604e3afd7161b3f
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-02-03 22:29:18 +0530

    add to Firebase database saving and error message section

--- app/src/main/java/com/hasthiya/chatapplication/activities/MainActivity.java
+++ app/src/main/java/com/hasthiya/chatapplication/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.hasthiya.chatapplication;
+package com.hasthiya.chatapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.hasthiya.chatapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
