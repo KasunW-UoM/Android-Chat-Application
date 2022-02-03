3cc001a147b73cb9f94718e3a7d0d81d3e2d93e4
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-02-03 16:09:41 +0530

    add to firestore ,FCM and google Cloud messaging Sction

--- app/src/main/java/com/hasthiya/chatapplication/activities/SignInActivity.java
+++ app/src/main/java/com/hasthiya/chatapplication/activities/SignInActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.hasthiya.chatapplication.R;
+//import android.widget.Toast;
+//
+//import com.google.firebase.firestore.FirebaseFirestore;
 import com.hasthiya.chatapplication.databinding.ActivitySigninBinding;
+
+//import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     
@@ -24,5 +27,30 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners(){
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+
+//        binding.buttonSignIn.setOnClickListener(v -> addDataToFirebase());
     }
+
+//    Users Collection to firestore
+
+//    private void addDataToFirebase(){
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String,Object> data =new HashMap<>();
+//
+//        data.put("first_name","Kasun");
+//        data.put("last_name","Weerasekara");
+//
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//
+//                    Toast.makeText(getApplicationContext(),"Data Inserted", Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception ->{
+//
+//                    Toast.makeText(getApplicationContext(),exception.getMessage(),Toast.LENGTH_SHORT).show();
+//
+//               });
+//
+//    }
 }
