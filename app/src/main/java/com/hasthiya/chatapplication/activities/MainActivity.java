63a61d2cb11bfb9e60858ec193eb480af8411c93
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-02-04 22:43:38 +0530

    Add to user select Section

--- app/src/main/java/com/hasthiya/chatapplication/activities/MainActivity.java
+++ app/src/main/java/com/hasthiya/chatapplication/activities/MainActivity.java
@@ -35,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setListeners() {
+
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),UsersActivity.class)));
     }
 
     private void loadUserDetails() {
@@ -59,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN,token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+//                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
