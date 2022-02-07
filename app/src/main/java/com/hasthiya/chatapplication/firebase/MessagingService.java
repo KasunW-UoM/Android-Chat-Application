e71d50936f7d713964fd97fa6dafb7a59e5bff91
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-02-07 23:51:53 +0530

    add to notification alert section

--- app/src/main/java/com/hasthiya/chatapplication/firebase/MessagingService.java
+++ app/src/main/java/com/hasthiya/chatapplication/firebase/MessagingService.java
@@ -2,10 +2,24 @@ package com.hasthiya.chatapplication.firebase;
 
 //import android.util.Log;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.hasthiya.chatapplication.R;
+import com.hasthiya.chatapplication.activities.ChatActivity;
+import com.hasthiya.chatapplication.models.User;
+import com.hasthiya.chatapplication.utilities.Constants;
+
+import java.util.Random;
 
 public class MessagingService extends FirebaseMessagingService {
 
@@ -20,6 +34,43 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        User user = new User();
+        user.id = remoteMessage.getData().get(Constants.KEY_USER_ID);
+        user.name =remoteMessage.getData().get(Constants.KEY_NAME);
+        user.token = remoteMessage.getData().get(Constants.KEY_FCM_TOKEN);
+
+        int notificationId = new Random().nextInt();
+        String channelId = "chat_message";
 //        Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
+
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra(Constants.KEY_USER, user);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,channelId);
+        builder.setSmallIcon(R.drawable.ic_notifications);
+        builder.setContentTitle(user.name);
+        builder.setContentText(remoteMessage.getData().get(Constants.KEY_MESSAGE));
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(
+                remoteMessage.getData().get(Constants.KEY_MESSAGE)
+        ));
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Chat Message";
+            String channelDescription = "This notification channel is used for chat message notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, channelName,importance);
+            channel.setDescription(channelDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(notificationId,builder.build());
+
     }
 }
